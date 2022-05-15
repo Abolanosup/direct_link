f4b065d2b858a80d4df58fc74f6e0df5db2ed65e
Wilfredovarona13 <102826193+Wilfredovarona13@users.noreply.github.com>
2022-05-15 00:26:10 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,5 +1,5 @@
-api_hash = "apihash"
-api_id = 1736372819
+api_hash = "7a72dc347e617f7a8ba63a96fdb1865d"
+api_id = 18638524
 bot_token = "5397329575:AAGveAO5v31OiaTM_vWxGYptpzxnxYYNki0"
 authorized_users = []
 admin_user = 'yunior132001'
